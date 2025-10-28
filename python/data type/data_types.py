f37fd6ff0e7ce931c5190924c21ab437ee2d5aea
prabu <ejsprabu676@gmail.com>
2025-10-28 17:10:56 +0530

    data types print finished

---
+++ python/data type/data_types.py
@@ -0,0 +1,34 @@
+name = "Jaysachithananthaprabu E"
+age = 23
+height = 173.5
+weight = 80.3
+city =  "Sayalkudi"
+is_student = False
+marks = {"Tamil":87,"Enlish":88,"Math":75,"Science":86,"S.Science":91}
+pi = 3.14
+school_name = "Govt Boys Hr Sec School"
+
+print(name)
+print(age)
+print(height)
+print(weight)
+print(city)
+print(is_student)
+print(marks)
+print(pi)
+print(school_name)
+print("***********************************************************************\n")
+
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+print("***********************************************************************\n")
+
+my_number = 10
+
+print(f" Value is {my_number} type is {type(my_number)}")
+
+my_text = "Hello"
+
+print(f" Value is {my_text} type is {type(my_text)}")
